63d179a82c3e5d6eb19c6c63b0cc46463bcc45f0
Nikolay Nedyalkov <n.nedialkov@stenik.bg>
2023-05-28 19:03:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/businesstrip/ui/employee/EmployeeFragment.java
@@ -0,0 +1,38 @@
+package com.example.businesstrip.ui.employee;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.businesstrip.R;
+
+public class EmployeeFragment extends Fragment {
+
+    private EmployeeViewModel mViewModel;
+
+    public static EmployeeFragment newInstance() {
+        return new EmployeeFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_employee, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(EmployeeViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/businesstrip/ui/employee/EmployeeViewModel.java
@@ -0,0 +1,7 @@
+package com.example.businesstrip.ui.employee;
+
+import androidx.lifecycle.ViewModel;
+
+public class EmployeeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
